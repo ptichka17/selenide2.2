48e3fe38284a4ac7c04cf7bd115e88edd014b0c2
птичка <Sapunova270301@gmail.com>
2023-06-17 10:04:06 +0300

    change scr/

--- src/test/java/ru/netology/selenide/CardDeliverySelenideTest.java
+++ src/test/java/ru/netology/selenide/CardDeliverySelenideTest.java
@@ -1,4 +1,4 @@
-package ru.netology;
+package ru.netology.selenide;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CardDeliveryOrder {
+public class CardDeliverySelenideTest {
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
@@ -20,7 +20,7 @@ public class CardDeliveryOrder {
     public void ShouldBeSent() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Пенза");
-        String currentDate = generateDate(4,"dd.MM.yyyy");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Анастасия");
@@ -30,3 +30,5 @@ public class CardDeliveryOrder {
         $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
 }
+
+
